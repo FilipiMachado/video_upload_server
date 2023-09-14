2172070ce287820fa3f32cb0477baa8de0bbe47a
filipi <filipisee@hotmail.com>
2023-09-13 21:48:05 -0300

    finished db config && get prompts route

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import { fastify } from "fastify";
 
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello World";
-});
+app.register(getAllPromptsRoute)
 
 app
-  .listen({
+  .listen({ 
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP server listening on port 3333");
+    console.log("HTTP Server Running!");
   });
